059a69d4e8d411faa55c0ead588167f7f76823dc
AlexBennett <alexanderbondbennett@gmail.com>
2020-04-15 12:29:01 -0400

    adding navigation and styling

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About Camille Cushman</h2>
+    <p>
+      She&apos;s from Ashland MA and she&apos;s going to Harvard to get her PhD.
+    </p>
+  </div>
+);
+
+export default AboutPage;
---
+++ src/components/home/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>Camille Cushman LLC</h1>
+    <p>She&apos;s the bees knees</p>
+    <Link to="about" className="btn btn-primary btn-lg">
+      Learn more about her
+    </Link>
+  </div>
+);
+
+export default HomePage;
